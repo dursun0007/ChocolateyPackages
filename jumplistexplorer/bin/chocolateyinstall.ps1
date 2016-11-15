d13d1affb274d84b9a29201526c276be816c29f8
Eric <saericzimmerman@gmail.com>
2016-11-15 10:34:29 -0500

    tweaks

--- jumplistexplorer/bin/chocolateyinstall.ps1
+++ jumplistexplorer/bin/chocolateyinstall.ps1
@@ -1,2 +1,7 @@
-﻿
+﻿$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
+
+$link = Join-Path $desktop "JumpList Explorer.lnk"
+$target = Join-Path $env:chocolateyPackageFolder "bin\JumpListExplorer.exe"
+$work = Join-Path $env:chocolateyPackageFolder "bin"
+Install-ChocolateyShortcut -ShortcutFilePath $link -TargetPath $target -WorkingDirectory $work -RunAsAdmin
 
