622c7664df7015229447d83260d66c7bb9ccd4ac
Eric <saericzimmerman@gmail.com>
2016-11-16 11:52:30 -0500

    iis

--- iisgeolocate/tools/chocolateyinstall.ps1
+++ iisgeolocate/tools/chocolateyinstall.ps1
@@ -1,2 +1,6 @@
-﻿
+﻿$checksum      = '71F3B67409DCE1F1D6F944D00EBD43CDBFC6F09A8439C2A2B8A647D24532248E'
+$checksumType  = 'sha256'
 
+$unzipTo = Join-Path $env:chocolateyPackageFolder "tools"
+
+Install-ChocolateyZipPackage -PackageName 'iisgeolocate' https://ericzimmerman.github.io/Software/iisGeolocate.zip -UnzipLocation $unzipTo -Checksum $checksum -ChecksumType $checksumType
