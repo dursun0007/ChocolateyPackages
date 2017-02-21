f980a1ffc5cbc3dd5cb1950754697a2ef8fa7c5a
Eric <saericzimmerman@gmail.com>
2017-02-21 09:59:39 -0500

    Cleanup

--- bstrings/tools/chocolateyuninstall.ps1
+++
@@ -1,4 +0,0 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
-
-$packageName = 'bstrings'
-$softwareName = 'bstrings*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
--- iisgeolocate/tools/chocolateyuninstall.ps1
+++
@@ -1,4 +0,0 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
-
-$packageName = 'iisgeolocate'
-$softwareName = 'iisgeolocate*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
