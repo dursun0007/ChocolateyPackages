b4fd5bc025ba0766b984c8adad5ab515bfa9b22f
Eric Zimmerman <saericzimmerman@gmail.com>
2018-02-09 09:41:02 -0500

    sdb explorer

---
+++ sdbexplorer/tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
+
+$link = Join-Path $desktop "SDB Explorer.lnk"
+$target = Join-Path $env:chocolateyPackageFolder "tools\SDBExplorer.exe"
+$work = Join-Path $env:chocolateyPackageFolder "tools"
+Install-ChocolateyShortcut -ShortcutFilePath $link -TargetPath $target -WorkingDirectory $work -RunAsAdmin
+
---
+++ sdbexplorer/tools/chocolateyuninstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName = 'sdbexplorer'
+$softwareName = 'sdbexplorer*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+
+$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
+
+$link = Join-Path $desktop "SDB Explorer.lnk"
+
+Remove-Item $link
