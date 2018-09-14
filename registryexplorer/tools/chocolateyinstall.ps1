d387c51f641027bb5640985bb900896c6c1efaa2
Eric Zimmerman <saericzimmerman@gmail.com>
2018-09-14 08:17:38 -0400

    updates to everything

--- registryexplorer/tools/chocolateyinstall.ps1
+++ registryexplorer/tools/chocolateyinstall.ps1
@@ -1,6 +1,13 @@
-﻿$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
+﻿$checksum      = 'd8ba27bcc438124cdd0502c6d6ca8b4f6e8acb432d81cbf62ff80075e6328672'
+$checksumType  = 'sha256'
+
+$unzipTo = Join-Path $env:chocolateyPackageFolder "tools"
+
+Install-ChocolateyZipPackage -PackageName 'registryexplorer' https://ericzimmerman.github.io/Software/RegistryExplorer_RECmd.zip -UnzipLocation $unzipTo -Checksum $checksum -ChecksumType $checksumType
+
+$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
 
 $link = Join-Path $desktop "Registry Explorer.lnk"
-$target = Join-Path $env:chocolateyPackageFolder "tools\RegistryExplorer_RECmd\RegistryExplorer.exe"
-$work = Join-Path $env:chocolateyPackageFolder "tools\RegistryExplorer_RECmd"
+$target = Join-Path $env:chocolateyPackageFolder "tools\RegistryExplorer.exe"
+$work = Join-Path $env:chocolateyPackageFolder "tools"
 Install-ChocolateyShortcut -ShortcutFilePath $link -TargetPath $target -WorkingDirectory $work -RunAsAdmin
