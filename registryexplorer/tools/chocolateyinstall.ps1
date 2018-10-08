1a7b769d930bcb783fae821839669f0e1396fb9f
Eric Zimmerman <saericzimmerman@gmail.com>
2018-10-08 18:50:41 -0400

    PECmd and TLE

--- registryexplorer/tools/chocolateyinstall.ps1
+++ registryexplorer/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-﻿$checksum      = '331782fdbaab0b7ff80170f7f5a3b8afbebeae5e27629ed30e0d0a51e0100781'
+﻿$checksum      = 'd59f27a1d02acca61c495e1ab84a82db7099f32d79b1524f8217ffaee7750e64'
 $checksumType  = 'sha256'
 
 $unzipTo = $env:chocolateyPackageFolder
