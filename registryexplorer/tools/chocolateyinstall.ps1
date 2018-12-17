d894ec7cc8499e59caaf20a7e31d621e557e141d
Eric Zimmerman <saericzimmerman@gmail.com>
2018-12-17 10:58:10 -0500

    add RE fux'

--- registryexplorer/tools/chocolateyinstall.ps1
+++ registryexplorer/tools/chocolateyinstall.ps1
@@ -1,13 +1,6 @@
-﻿$checksum      = 'd59f27a1d02acca61c495e1ab84a82db7099f32d79b1524f8217ffaee7750e64'
-$checksumType  = 'sha256'
-
-$unzipTo = $env:chocolateyPackageFolder
-
-Install-ChocolateyZipPackage -PackageName 'registryexplorer' https://ericzimmerman.github.io/Software/RegistryExplorer_RECmd.zip -UnzipLocation $unzipTo -Checksum $checksum -ChecksumType $checksumType
-
-$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
+﻿$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
 
 $link = Join-Path $desktop "Registry Explorer.lnk"
-$target = Join-Path $env:chocolateyPackageFolder "RegistryExplorer.exe"
-$work = $env:chocolateyPackageFolder 
+$target = Join-Path $env:chocolateyPackageFolder "tools\RegistryExplorer\RegistryExplorer.exe"
+$work = Join-Path $env:chocolateyPackageFolder "tools\RegistryExplorer"
 Install-ChocolateyShortcut -ShortcutFilePath $link -TargetPath $target -WorkingDirectory $work -RunAsAdmin
